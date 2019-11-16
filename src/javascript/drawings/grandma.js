5a489239eeff7f0bee9bef43b4730a9bff323306
tob <tobia.donati@stepstone.com>
2019-11-16 18:34:21 +0000

    Add grandma giff

--- src/javascript/drawings/grandma.js
+++ src/javascript/drawings/grandma.js
@@ -6,46 +6,29 @@ let y = 0;
 const center = { width: window.innerWidth / 2, height: window.innerHeight };
 
 function drawLoop(time) {
-	const gif = document.getElementById("gif");
   // check if we're currently clipping
-  // if (meter.checkClipping()) canvasContext.fillStyle = "white";
-  // else canvasContext.fillStyle = "#000000";
+  if (meter.checkClipping()) canvasContext.fillStyle = "white";
+  else canvasContext.fillStyle = "#000000";
 
   // VOLUME get normalized
+  const volume = Math.abs((Math.log(meter.volume)/Math.LN10)*20)*100/25;
   // const volume = Math.abs((Math.log(meter.volume)/Math.LN10)*20);
-  const volume = Math.abs((Math.log(meter.volume)/Math.LN10)*20)*100/20;
   // const volume = Math.abs(Math.floor(Math.log(meter.volume )*10));
   // if (volume >= 220 || volume <= 20) {
   //   console.log("Volume", volume);
   // }
   // const volume = Math.abs((Math.log(meter.volume)*255/100)*20);
 
-  // VIDEOS and GIF need to be inside the loop to be re-catched and rendered
+  // VIDEOS and GIF need to be inside the loop to be re-catch and rendered
   // const pat = canvasContext.createPattern(lake, "repeat");
   // const pat2 = canvasContext.createPattern(gif, "repeat");
   // const pat3 = canvasContext.createPattern(desert, "repeat");
   // const pat4 = canvasContext.createPattern(elmo, "repeat");
   // const patterns = [pat, pat2, pat3, pat4];
-	canvasContext.drawImage(gif, 100, 100)
   // const patterns = colours;
-
-  // counter++;
-  // if (counter >= colours.length - 1) {
-  //   counter = 0;
-  //   // clear the background
-  // }
-
-  // style = colours[counter];
 
   // CLEAR canvas
   // canvasContext.clearRect(0, 0, WIDTH, HEIGHT);
-
-  // canvasContext.fillStyle = style;
-  canvasContext.fillStyle = `rgb(
-        ${volume},
-        ${255 - volume},
-        ${volume})`;
-  // canvasContext.beginPath();
 
   // if (volume > 300) {
   //   style = patterns[0];
@@ -59,22 +42,30 @@ function drawLoop(time) {
   //   style = patterns[2];
   // }
 
-	x += volume/50;
-	if (x >= WIDTH) {
-		x = 20;
-		y = Math.floor(Math.random() * (HEIGHT - volume)) + volume;
-	}
-	if (y >= HEIGHT) {
-		y = 20;
-	}
 
-  if (volume > 120 && volume < 200) {
+  if (volume > 100 && volume < 200) {
     // style = patterns[4];
+
+	  x += volume;
+	  if (x >= WIDTH) {
+		  x = 20;
+		  y = Math.floor(Math.random() * (HEIGHT - volume)) + volume;
+	  }
+	  if (y >= HEIGHT) {
+		  y = 20;
+	  }
 
     canvasContext.beginPath();
     canvasContext.arc(WIDTH / 2, HEIGHT / 2, volume, 0, 2 * Math.PI);
     canvasContext.fill();
   }
+
+	canvasContext.fillStyle = `rgb(
+        ${volume},
+        ${255 - volume},
+        ${volume})`;
+
+	// canvasContext.fillStyle = style;
 
   // TOP CIRCLE
   canvasContext.beginPath();
